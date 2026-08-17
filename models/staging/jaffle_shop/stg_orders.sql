cef009648b9273b539e1b1eb7e8dcb5ce684c035
justinmichaelbondoc <justin.bondoc@fivetran.com>
2026-08-17 14:19:29 -0700

    applies unit testing with intentional errors

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,7 +1,26 @@
-select
+with
+
+source as (
+
+    select * from {{ source('jaffle_shop', 'orders')}}
+),
+
+staged as (
+
+    select
     id as order_id,
     user_id as customer_id,
     order_date,
-    status
+    datediff('day', order_date, {{ dbt.current_timestamp() }} ) as days_since_ordered,
+    status like '%pending%' as is_status_pending,
+    case
+        when status like '%pending%' then 'placed'
+        when status like '%return%' then 'returned'
+        else status
+    end as status
+from source
 
-from {{ source('jaffle_shop','orders') }}
+)
+
+select * from staged
+
