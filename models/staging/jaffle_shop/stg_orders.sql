e42934a242af180f2e71a2e40e955584bf3ce52d
justinmichaelbondoc <justin.bondoc@fivetran.com>
2026-08-17 14:42:34 -0700

    More unit tests from the dev path

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -14,6 +14,7 @@ staged as (
     datediff('day', order_date, {{ dbt.current_timestamp() }} ) as days_since_ordered,
     status like '%pending%' as is_status_pending,
     case
+        when status like '%shipped%' then 'shipped'
         when status like '%return%' then 'returned'
         when status like '%pending%' then 'placed'
         else status
